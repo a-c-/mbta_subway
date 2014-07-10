55adb75568f31ee6a9f775fb7d64ad81bb34c7fd
Ashley Custer <ashleyfcuster@gmail.com>
2014-07-10 14:59:52 -0400

    json-related only

--- alerts/models.py
+++ alerts/models.py
@@ -1,135 +1,135 @@
 from django.db import models
 
-
-# from MBTA_GTFS files
-
-class Agency(models.Model):
-    agency_id = models.CharField(max_length=20, primary_key=True)
-    agency_name = models.CharField(max_length=50)
-    agency_url = models.CharField(max_length=50)
-    agency_timezone = models.CharField(max_length=50)
-    agency_lang = models.CharField(max_length=50)
-    agency_phone = models.CharField(max_length=50)
-
-    def __unicode__(self):
-        return self.agency_name
-
-class CalendarDate(models.Model):
-    service_id = models.CharField(max_length=200)
-    date = models.CharField(max_length=10)
-    exception_type = models.CharField(max_length=50)
-
-    def __unicode__(self):
-        return self.service_id
-
-class Calendar(models.Model):
-    service_id = models.CharField(max_length=200, primary_key=True)
-    monday = models.CharField(max_length=50)
-    tuesday = models.CharField(max_length=50)
-    wednesday = models.CharField(max_length=50)
-    thursday = models.CharField(max_length=50)
-    friday = models.CharField(max_length=50)
-    saturday = models.CharField(max_length=50)
-    sunday = models.CharField(max_length=50)
-    start_date = models.CharField(max_length=10)
-    end_date = models.CharField(max_length=10)
-
-    def __unicode__(self):
-        return self.service_id
-
-class FeedInfo(models.Model):
-    feed_publisher_name = models.CharField(max_length=20)
-    feed_publisher_url = models.CharField(max_length=200)
-    feed_lang = models.CharField(max_length=10)
-    feed_start_date = models.CharField(max_length=10)
-    feed_end_date = models.CharField(max_length=10)
-    feed_version = models.CharField(max_length=300)
-
-    def __unicode__(self):
-        return self.feed_publisher_name
-
-class Frequency(models.Model):
-    trip_id = models.CharField(max_length=200)
-    start_time = models.CharField(max_length=20)
-    end_time = models.CharField(max_length=20)
-    headway_secs = models.CharField(max_length=50)
-
-    def __unicode__(self):
-        return self.trip_id
-
-class Route(models.Model):
-    route_id = models.CharField(max_length=200, primary_key=True)
-    agency_id = models.CharField(max_length=20)
-    route_short_name = models.CharField(max_length=20)
-    route_long_name = models.CharField(max_length=100)
-    route_desc = models.CharField(max_length=200)
-    route_type = models.CharField(max_length=50)
-    route_url = models.CharField(max_length=200)
-    route_color = models.CharField(max_length=10)
-    route_text_color = models.CharField(max_length=10)
-
-    def __unicode__(self):
-        return self.route_id
-
-class Shape(models.Model):
-    shape_id = models.CharField(max_length=10)
-    shape_pt_lat = models.CharField(max_length=20)
-    shape_pt_lon = models.CharField(max_length=20)
-    shape_pt_sequence = models.CharField(max_length=50)
-    shape_dist_traveled = models.CharField(max_length=20)
-
-    def __unicode__(self):
-        return self.shape_id
-
-class Stop(models.Model):
-    stop_id = models.CharField(max_length=100, primary_key=True)
-    stop_code = models.CharField(max_length=50)
-    stop_name = models.CharField(max_length=200)
-    stop_desc = models.CharField(max_length=200)
-    stop_lat = models.CharField(max_length=20)
-    stop_lon = models.CharField(max_length=20)
-    zone_id = models.CharField(max_length=20)
-    stop_url = models.URLField()
-    location_type =  models.CharField(max_length=50)
-    parent_station = models.CharField(max_length=50)
-
-    def __unicode__(self):
-        return self.stop_id
-
-class StopTime(models.Model):
-    trip_id = models.CharField(max_length=20)
-    arrival_time = models.CharField(max_length=10)
-    departure_time = models.CharField(max_length=10)
-    stop_id = models.CharField(max_length=100)
-    stop_sequence = models.CharField(max_length=10)
-    stop_headsign =  models.CharField(max_length=50)
-    pickup_type =  models.CharField(max_length=50)
-    drop_off_type =  models.CharField(max_length=50)
-
-    def __unicode__(self):
-        return self.trip_id
-
-class Transfer(models.Model):
-    from_stop_id = models.CharField(max_length=100)
-    to_stop_id = models.CharField(max_length=100)
-    transfer_type = models.CharField(max_length=50)
-    min_transfer_time =  models.CharField(max_length=50)
-
-    def __unicode__(self):
-        return self.from_stop_id
-
-class Trip(models.Model):
-    route_id = models.CharField(max_length=100)
-    service_id = models.CharField(max_length=200)
-    trip_id = models.CharField(max_length=30, primary_key=True)
-    trip_headsign = models.CharField(max_length=100)
-    trip_short_name = models.CharField(max_length=50)
-    direction_id = models.CharField(max_length=50)
-    block_id = models.CharField(max_length=50)
-    shape_id = models.CharField(max_length=10)
-
-    def __unicode__(self):
-        return self.route_id
+#
+# # from MBTA_GTFS files
+#
+# class Agency(models.Model):
+#     agency_id = models.CharField(max_length=20, primary_key=True)
+#     agency_name = models.CharField(max_length=50)
+#     agency_url = models.CharField(max_length=50)
+#     agency_timezone = models.CharField(max_length=50)
+#     agency_lang = models.CharField(max_length=50)
+#     agency_phone = models.CharField(max_length=50)
+#
+#     def __unicode__(self):
+#         return self.agency_name
+#
+# class CalendarDate(models.Model):
+#     service_id = models.CharField(max_length=200)
+#     date = models.CharField(max_length=10)
+#     exception_type = models.CharField(max_length=50)
+#
+#     def __unicode__(self):
+#         return self.service_id
+#
+# class Calendar(models.Model):
+#     service_id = models.CharField(max_length=200, primary_key=True)
+#     monday = models.CharField(max_length=50)
+#     tuesday = models.CharField(max_length=50)
+#     wednesday = models.CharField(max_length=50)
+#     thursday = models.CharField(max_length=50)
+#     friday = models.CharField(max_length=50)
+#     saturday = models.CharField(max_length=50)
+#     sunday = models.CharField(max_length=50)
+#     start_date = models.CharField(max_length=10)
+#     end_date = models.CharField(max_length=10)
+#
+#     def __unicode__(self):
+#         return self.service_id
+#
+# class FeedInfo(models.Model):
+#     feed_publisher_name = models.CharField(max_length=20)
+#     feed_publisher_url = models.CharField(max_length=200)
+#     feed_lang = models.CharField(max_length=10)
+#     feed_start_date = models.CharField(max_length=10)
+#     feed_end_date = models.CharField(max_length=10)
+#     feed_version = models.CharField(max_length=300)
+#
+#     def __unicode__(self):
+#         return self.feed_publisher_name
+#
+# class Frequency(models.Model):
+#     trip_id = models.CharField(max_length=200)
+#     start_time = models.CharField(max_length=20)
+#     end_time = models.CharField(max_length=20)
+#     headway_secs = models.CharField(max_length=50)
+#
+#     def __unicode__(self):
+#         return self.trip_id
+#
+# class Route(models.Model):
+#     route_id = models.CharField(max_length=200, primary_key=True)
+#     agency_id = models.CharField(max_length=20)
+#     route_short_name = models.CharField(max_length=20)
+#     route_long_name = models.CharField(max_length=100)
+#     route_desc = models.CharField(max_length=200)
+#     route_type = models.CharField(max_length=50)
+#     route_url = models.CharField(max_length=200)
+#     route_color = models.CharField(max_length=10)
+#     route_text_color = models.CharField(max_length=10)
+#
+#     def __unicode__(self):
+#         return self.route_id
+#
+# class Shape(models.Model):
+#     shape_id = models.CharField(max_length=10)
+#     shape_pt_lat = models.CharField(max_length=20)
+#     shape_pt_lon = models.CharField(max_length=20)
+#     shape_pt_sequence = models.CharField(max_length=50)
+#     shape_dist_traveled = models.CharField(max_length=20)
+#
+#     def __unicode__(self):
+#         return self.shape_id
+#
+# class Stop(models.Model):
+#     stop_id = models.CharField(max_length=100, primary_key=True)
+#     stop_code = models.CharField(max_length=50)
+#     stop_name = models.CharField(max_length=200)
+#     stop_desc = models.CharField(max_length=200)
+#     stop_lat = models.CharField(max_length=20)
+#     stop_lon = models.CharField(max_length=20)
+#     zone_id = models.CharField(max_length=20)
+#     stop_url = models.URLField()
+#     location_type =  models.CharField(max_length=50)
+#     parent_station = models.CharField(max_length=50)
+#
+#     def __unicode__(self):
+#         return self.stop_id
+#
+# class StopTime(models.Model):
+#     trip_id = models.CharField(max_length=20)
+#     arrival_time = models.CharField(max_length=10)
+#     departure_time = models.CharField(max_length=10)
+#     stop_id = models.CharField(max_length=100)
+#     stop_sequence = models.CharField(max_length=10)
+#     stop_headsign =  models.CharField(max_length=50)
+#     pickup_type =  models.CharField(max_length=50)
+#     drop_off_type =  models.CharField(max_length=50)
+#
+#     def __unicode__(self):
+#         return self.trip_id
+#
+# class Transfer(models.Model):
+#     from_stop_id = models.CharField(max_length=100)
+#     to_stop_id = models.CharField(max_length=100)
+#     transfer_type = models.CharField(max_length=50)
+#     min_transfer_time =  models.CharField(max_length=50)
+#
+#     def __unicode__(self):
+#         return self.from_stop_id
+#
+# class Trip(models.Model):
+#     route_id = models.CharField(max_length=100)
+#     service_id = models.CharField(max_length=200)
+#     trip_id = models.CharField(max_length=30, primary_key=True)
+#     trip_headsign = models.CharField(max_length=100)
+#     trip_short_name = models.CharField(max_length=50)
+#     direction_id = models.CharField(max_length=50)
+#     block_id = models.CharField(max_length=50)
+#     shape_id = models.CharField(max_length=10)
+#
+#     def __unicode__(self):
+#         return self.route_id
 
 
 
@@ -161,37 +161,37 @@ class Prediction(models.Model):
     trip = models.ForeignKey(Trip)
 
 
-# from GTFS-realtime alerts data
-
-class ActivePeriod(models.Model):
-    start = models.PositiveIntegerField()
-    end = models.PositiveIntegerField()
-
-class InformedEntity(models.Model):
-    agency_id = models.ForeignKey(Agency)
-    route_id = models.CharField(max_length=20)
-    route_type = models.PositiveIntegerField()
-
-class Alert(models.Model):
-    active_period = models.ManyToManyField(ActivePeriod)
-    informed_entity = models.ForeignKey(InformedEntity)
-    cause = models.CharField(max_length=20)
-    effect = models.CharField(max_length=20)
-    header_text = models.CharField(max_length=256)
-    description_text = models.TextField()
-
-class Entity(models.Model):
-    entity_id = models.CharField(max_length=20)
-    alert = models.ForeignKey(Alert)
-
-
-# additonal classes
-
-class Distance(models.Model):
-    train_first = models.ForeignKey(TripItem, related_name='train1')
-    train_last = models.ForeignKey(TripItem, related_name='train2')
-    distance = models.DecimalField(max_digits=12,decimal_places=2)
-    velocity = models.DecimalField(max_digits=12,decimal_places=5)
+# # from GTFS-realtime alerts data
+#
+# class ActivePeriod(models.Model):
+#     start = models.PositiveIntegerField()
+#     end = models.PositiveIntegerField()
+#
+# class InformedEntity(models.Model):
+#     agency_id = models.ForeignKey(Agency)
+#     route_id = models.CharField(max_length=20)
+#     route_type = models.PositiveIntegerField()
+#
+# class Alert(models.Model):
+#     active_period = models.ManyToManyField(ActivePeriod)
+#     informed_entity = models.ForeignKey(InformedEntity)
+#     cause = models.CharField(max_length=20)
+#     effect = models.CharField(max_length=20)
+#     header_text = models.CharField(max_length=256)
+#     description_text = models.TextField()
+#
+# class Entity(models.Model):
+#     entity_id = models.CharField(max_length=20)
+#     alert = models.ForeignKey(Alert)
+#
+#
+# # additonal classes
+#
+# class Distance(models.Model):
+#     train_first = models.ForeignKey(TripItem, related_name='train1')
+#     train_last = models.ForeignKey(TripItem, related_name='train2')
+#     distance = models.DecimalField(max_digits=12,decimal_places=2)
+#     velocity = models.DecimalField(max_digits=12,decimal_places=5)
 
 
 
