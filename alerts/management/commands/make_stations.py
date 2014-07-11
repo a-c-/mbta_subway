fdee38441708bb9f44b999dac66545c02cbaf1a0
Ashley Custer <ashleyfcuster@gmail.com>
2014-07-11 10:50:58 -0400

    working

--- alerts/management/commands/make_stations.py
+++ alerts/management/commands/make_stations.py
@@ -2,8 +2,10 @@ from django.core.management.base import BaseCommand, CommandError
 # from alerts.models import Station, DelayRuleset, Route
 import csv
 import os
-from alerts.models import Agency, CalendarDate, Calendar, FeedInfo, Frequency, Route, Shape, Stop, StopTime, Transfer, Trip
-
+# from alerts.models import Agency, CalendarDate, Calendar, FeedInfo, Frequency, Route, Shape, Stop, StopTime, Transfer, Trip
+from alerts.models import Info, Trip, Position, Prediction
+import json
+import urllib2
 
 # class Command(BaseCommand):
 #     help = 'populates MBTA_GTFS tables'
@@ -28,71 +30,71 @@ from alerts.models import Agency, CalendarDate, Calendar, FeedInfo, Frequency, R
 #                 # print row
 #                 Model.objects.create(**row)
 
+def log_subway(data):
+
+    # add data to Info table
+    current_time_in = data['TripList']['CurrentTime']
+    line_in = data['TripList']['Line']
+    info_in = Info.objects.create(
+        current_time = current_time_in,
+        line = line_in
+    )
+
+    for trip in data['TripList']['Trips']:
+        trip_id_in = trip['TripID']
+        destination_in = trip['Destination']
+        note_in = trip.get('Note')
+        #relate to Info object here
+        trip_in = Trip.objects.create(
+            trip_id = trip_id_in,
+            destination = destination_in,
+            note = note_in,
+            info = info_in
+        )
+
+        position = trip.get('Position')
+        if position is not None:
+            timestamp_in = position['Timestamp']
+            train_in = position['Train']
+            lat_in = position['Lat']
+            lon_in = position['Long']
+            heading_in = position['Heading']
+            # relate to Trip object
+            Position.objects.create(
+                timestamp = timestamp_in,
+                train = train_in,
+                latitude = lat_in,
+                longitude = lon_in,
+                heading = heading_in,
+                trip = trip_in
+            )
+
+        for prediction in trip['Predictions']:
+            stop_id_in = prediction['StopID']
+            stop_in = prediction['Stop']
+            seconds_in = prediction['Seconds']
+            # relate to Trip object
+            Prediction.objects.create(
+                stop_id = stop_id_in,
+                stop = stop_in,
+                seconds = seconds_in,
+                trip = trip_in
+            )
+
+
 
 class Command(BaseCommand):
     help = 'populates subway realtime data tables'
 
-    def log_subway(data):
-
-        # add data to Info table
-        current_time_in = data['TripList']['CurrentTime']
-        line_in = data['TripList']['Line']
-        Info.objects.create(
-            current_time = current_time_in,
-            line = line_in
-        )
-
-        for trip in data['TripList']['Trips']:
-            trip_id_in = trip['TripID']
-            destination_in = trip['Destination']
-            note_in = trip['Note']
-            #relate to Info object here
-            Trip.objects.create(
-                trip_id = trip_id_in,
-                destination = destination_in,
-                note = note_in
-            )
-
-            for position in trip['Position']:
-                timestamp_in = position['Timestamp']
-                train_in = position['Train']
-                lat_in = position['Lat']
-                lon_in = position['Long']
-                heading_in = position['Heading']
-                # relate to Trip object
-                Position.objects.create(
-                    timestamp = timestamp_in,
-                    train = train_in,
-                    latitude = lat_in,
-                    longitude = lon_in,
-                    heading = heading_in
-                )
-
-            for prediction in trip['Predictions']:
-                stop_id_in = prediction['StopID']
-                stop_in = prediction['Stop']
-                seconds_in = prediction['Seconds']
-                # relate to Trip object
-                Prediction.objects.create(
-                    stop_id = stop_id_in,
-                    stop = stop_in,
-                    seconds = seconds_in
-                )
-
-
-
     def handle(self, *args, **options):
+        key = 'red'
         link_to_data = "http://developer.mbta.com/lib/rthr/{}.json".format(key)
 
         # open data
         data1 = urllib2.urlopen(link_to_data).read()
         data2 = json.loads(data1)
 
-        # get time (in epoch)
-        time1 = data2['TripList']['CurrentTime']
-
-        # build station prediction data structure
-        stations = log_subway(data2)
+        log_subway(data2)
 
 
 
